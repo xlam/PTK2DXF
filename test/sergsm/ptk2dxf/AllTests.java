485b99d169d42781e5f1d205d65121fb22909c5b
Sergey <sergsm@serg>
2015-03-23 16:57:22 +0400

    Added initial testing stuff

---
+++ test/sergsm/ptk2dxf/AllTests.java
@@ -0,0 +1,12 @@
+package sergsm.ptk2dxf;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    ParserTest.class
+})
+public class AllTests {
+
+}
---
+++ test/sergsm/ptk2dxf/ParserTest.java
@@ -0,0 +1,30 @@
+package sergsm.ptk2dxf;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Sergey
+ */
+public class ParserTest {
+
+    public ParserTest() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testParser() {
+        assertTrue(true);
+    }
+
+}
