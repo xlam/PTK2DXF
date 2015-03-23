efb64b1bf26dff42053196d4c1cbdd3f280e8532
Sergey <sergsm@serg>
2015-03-23 16:56:20 +0400

    Added Parser and Vertex classes (initial)

--- src/sergsm/ptk2dxf/Parser.java
+++ src/sergsm/ptk2dxf/Parser.java
@@ -1,9 +1,25 @@
 package sergsm.ptk2dxf;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Parses CSV file into array of polylines
  * @author Sergey
  */
 public class Parser {
+    private final String CSVFileName = "./workdir/table.csv";
 
+    public Parser() {
+
+    }
+
+    public List parse() {
+        List polylines = new ArrayList();
+        List vertexes = new ArrayList();
+        vertexes.add(new Vertex(10, 20));
+        vertexes.add(new Vertex(20, 30));
+        polylines.add(vertexes);
+        return polylines;
+    }
 }
---
+++ src/sergsm/ptk2dxf/Vertex.java
@@ -0,0 +1,31 @@
+package sergsm.ptk2dxf;
+
+/**
+ *
+ * @author Sergey
+ */
+class Vertex {
+    private int x;
+    private int y;
+
+    public Vertex(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
