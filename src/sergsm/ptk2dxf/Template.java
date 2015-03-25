61bf72af816320be76abdc910f0e2e03e20a9f87
Sergey <sergsm@serg>
2015-03-25 17:03:50 +0400

    Bundle commit (work-home)

--- src/sergsm/ptk2dxf/Template.java
+++ src/sergsm/ptk2dxf/Template.java
@@ -1,5 +1,8 @@
 package sergsm.ptk2dxf;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 
 /**
@@ -10,6 +13,7 @@ public class Template {
 
     private String filename;
     private HashMap<String, String> vars = new HashMap();
+    private StringBuilder stringBuilder = null;
 
     public Template(String filename) {
         this.filename = filename;
@@ -17,6 +21,25 @@ public class Template {
 
     public void set(String name, String value) {
         vars.put(name, value);
+    }
+
+    private void readTemplate() {
+        if (stringBuilder != null) return;
+        try {
+            BufferedReader r = new BufferedReader(new FileReader("./templates/" + filename + ".dtpl"));
+            while (r.ready()) {
+                stringBuilder.append(r.readLine());
+            }
+            r.close();
+        } catch (IOException e) {}
+    }
+
+    public void render() {
+        readTemplate();
+        String result = stringBuilder.toString();
+
+        result = result.replaceAll("%%POINT_X%%", "10.0");
+        result = result.replaceAll("%%POINT_Y%%", "20.0");
     }
 
     @Override
