58cf0f5e53ea1a72763b65b4fc3c3f90f4226542
Sergey <sergsm@serg>
2015-03-26 16:33:49 +0400

    Prepared first release

--- src/sergsm/ptk2dxf/Main.java
+++ src/sergsm/ptk2dxf/Main.java
@@ -15,12 +15,23 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+
+        System.out.println("PTK2DXF v" + Version.getVersion() + ".\n");
+        // Need to add arguments handling stuff (later maybe...)
+        if (args.length == 0) {
+            System.out.println("Provide filename");
+            System.exit(0);
+        }
+
+        String dxfFileName = args[0] + ".dxf";
+        System.out.println("Converting [" + args[0] + "] -> [" + dxfFileName + "]...");
+
         Parser csvParser = new Parser();
-        csvParser.setCSVFileName("input.csv");
+        csvParser.setCSVFileName(args[0]);
         ArrayList<Polyline> polylines = csvParser.parse();
 
         try {
-            FileWriter w = new FileWriter(new File("output.dxf"));
+            FileWriter w = new FileWriter(new File(dxfFileName));
             Template headerTpl = new Template("header");
             Template footerTpl = new Template("footer");
             Template vertexTpl = new Template("vertex");
